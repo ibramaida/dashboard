ac1e18a9a8448abe5db9d3714f3938f99ceb2b5c
Ibrahim Maida <ibrahim@o365.tijdelijkehosting.nl>
2022-06-22 00:03:00 +0200

    init nav link click feature

---
+++ navLink.js
@@ -0,0 +1,16 @@
+const navLinks = document.querySelectorAll(".nav-link");
+console.log(navLinks);
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", (e) => {
+    console.log(e.target);
+    navLinks.forEach((navLink) => {
+      navLink.classList.remove("active");
+    });
+    if (e.target == navLink) {
+      navLink.classList.add("active");
+    } else {
+      navLink.classList.remove("active");
+    }
+  });
+});
