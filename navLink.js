ec2f3ce97a90fd2fda952ee9f5fd3ae3289fc364
Ibrahim Maida <ibrahim@o365.tijdelijkehosting.nl>
2022-06-23 00:55:54 +0200

    init navlink functionalty

--- navLink.js
+++ navLink.js
@@ -4,13 +4,21 @@ const navLinks = document.querySelectorAll(".nav-link");
 navLinks.forEach((navLink) => {
   navLink.addEventListener("click", (e) => {
     // console.log(e.target);
+    // console.log(e.target.previousElementSibling);
+    // console.log(e.target.nextElementSibling);
     navLinks.forEach((navLink) => {
       navLink.classList.remove("active");
+      navLink.classList.remove("round-before");
+      navLink.classList.remove("round-after");
     });
     if (e.target == navLink) {
-      navLink.classList.add("active");
+      e.target.classList.add("active");
+      e.target.previousElementSibling.classList.add("round-before");
+      e.target.nextElementSibling.classList.add("round-after");
     } else {
-      navLink.classList.remove("active");
+      e.target.classList.remove("active");
+      e.target.previousElementSibling.classList.remove("round-before");
+      e.target.nextElementSibling.classList.remove("round-after");
     }
   });
 });
