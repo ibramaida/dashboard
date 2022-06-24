940c932270435c882f5746adee30eb257f638fb5
ibramaida <bmpcbob@gmail.com>
2022-06-24 13:10:08 +0200

    save primary nav mobile version

--- navLink.js
+++ navLink.js
@@ -8,17 +8,11 @@ navLinks.forEach((navLink) => {
     // console.log(e.target.nextElementSibling);
     navLinks.forEach((navLink) => {
       navLink.classList.remove("active");
-      navLink.classList.remove("round-before");
-      navLink.classList.remove("round-after");
     });
     if (e.target == navLink) {
       e.target.classList.add("active");
-      e.target.previousElementSibling.classList.add("round-before");
-      e.target.nextElementSibling.classList.add("round-after");
     } else {
       e.target.classList.remove("active");
-      e.target.previousElementSibling.classList.remove("round-before");
-      e.target.nextElementSibling.classList.remove("round-after");
     }
   });
 });
