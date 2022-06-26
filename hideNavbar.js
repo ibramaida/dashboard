52c194b79d1b880977ecf2bdc21eb3beacb46d1d
Ibrahim Maida <ibrahim@o365.tijdelijkehosting.nl>
2022-06-27 00:47:03 +0200

    save

--- hideNavbar.js
+++ hideNavbar.js
@@ -2,9 +2,6 @@ let lastScrollTop;
 navbar = document.querySelector(".header");
 window.addEventListener("scroll", function () {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  console.log(scrollTop);
-  console.log(lastScrollTop);
-  console.log(scrollTop > lastScrollTop);
   if (scrollTop > lastScrollTop) {
     navbar.style.bottom = "-100%";
   } else {
